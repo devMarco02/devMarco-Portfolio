6783235731f0c8bf0499eaca1991535fe4615e0a
devMarco.02 <devMarco.02@gmail.com>
2021-08-27 13:49:27 +0800

    portfolio-page - added style for touch screen device

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "../components/Modal";
 
 import data from "./../data/PortfolioPageData";
@@ -9,6 +9,7 @@ const PortfolioPage = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [link, setLink] = useState("");
+  const [isTouchEnabled, setIsTouchEnabled] = useState(false);
 
   const openProject = (targetId) => {
     const project = data.find((project) => project.id === targetId);
@@ -30,15 +31,47 @@ const PortfolioPage = () => {
     }
   };
 
+  // check if using touch screen
+  const checkIsTouchEnabled = () => {
+    if (
+      "ontouchstart" in Window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    ) {
+      setIsTouchEnabled(true);
+    } else {
+      setIsTouchEnabled(false);
+    }
+  };
+
+  //run once
+  useEffect(() => {
+    checkIsTouchEnabled();
+  }, []);
+
+  //run everytime window size change
+  useEffect(() => {
+    window.addEventListener("resize", checkIsTouchEnabled);
+    return () => {
+      window.removeEventListener("resize", checkIsTouchEnabled);
+    };
+  });
+
   return (
     <main className="portfolio-page">
-      <div className="portfolio-page__container">
+      <div
+        className={`portfolio-page__container ${
+          isTouchEnabled && "portfolio-page__container--gap"
+        }`}
+      >
         {data.map((project) => {
           const { id, title, image } = project;
           return (
             <div
               key={id}
-              className="portfolio-page__img-container"
+              className={`portfolio-page__img-container ${
+                isTouchEnabled && "portfolio-page__img-container--opaque"
+              }`}
               onClick={() => openProject(id)}
             >
               <img className="portfolio-page__img" src={image} alt={title} />
